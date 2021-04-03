f0422b3b3ce4874719374b56a84b4ca7d06378d1
Bradley Voytek <bradley.voytek@gmail.com>
2021-04-03 11:25:56 -0700

    Roby coins working with red background

--- path-convert.js
+++ path-convert.js
@@ -30,14 +30,61 @@
               'pp'
             ];
 
+            var robyCoinTypes = [
+              'Confirm',
+              'Market Confirm',
+              'Fair Confirm',
+              'Gold Confirm',
+              'Rachtmark'
+            ]
+
             var defaultCoinShift = -1;
             var defaultMultiplier = 2;
             var coinShiftDefecit = 0;
 
+            // must use confirms
+            function reduceRobyCoinValues(confirmAmount) {
+              // 1 confirm == 1 confrim
+              // 1 market confirm == 10 confirm
+              // 1 fair confirm == 20 confirm
+              // 1 gold confirm == 400 confirm
+              // 1 rachtmark == 24000 confirms
+
+              if (confirmAmount >= 24000) {
+                return [Math.ceil(confirmAmount/24000), 'Rachtmark'];
+              } else if (confirmAmount >= 400) {
+                return [Math.ceil(confirmAmount/400), 'Gold Confirm'];
+              } else if (confirmAmount >= 20) {
+                return [Math.ceil(confirmAmount/20), 'Fair Confirm'];
+              } else {
+                return [confirmAmount, 'Confirms'];
+              }
+            }
+
+            function exchangeCoinValues(amount, units) {
+              var newValues = [amount, units];
+
+              // convert all values to smallest denomination of roby bucks aka "confirms"
+              // change to copper pieces (powers of 10 1sp == 10cp)
+              // find how many steps to copper pieces
+              var coinPower = coinTypes.indexOf(units);
+              var copperValue = amount * (10**coinPower);
+
+              // convert to confirms (multiply by 2)
+              var confirmAmount = copperValue * 2;
+
+              // reduce values to fewest coins and round up
+              var robyCoins = reduceRobyCoinValues(confirmAmount);
+              var robyCounAmount = robyCoins[0];
+              var robyCounUnits = robyCoins[1];
+
+              newValues = [robyCounAmount, robyCounUnits];
+
+              return newValues;
+            }
+
             function findCoinShift (units) {
               var coinIndex = coinTypes.indexOf(units);
-              console.log('coin index ' + coinIndex);
-
               var coinShift = coinIndex + defaultCoinShift;
               var numberCoinTypes = coinTypes.length;
               var lastCoinIndex = coinTypes.length - 1;
@@ -78,23 +125,29 @@
 
               var coinShift = findCoinShift(units);
               var multiplier = findMultiplier();
-              console.log(coinShift);
-              console.log(coinShiftDefecit);
-              console.log(multiplier);
 
-              return amount * multiplier + ' ' + coinTypes[coinShift];
+              console.log('original amount = ' + amount + ' ' + units);
+              console.log('coinShift = ' + coinShift);
+              console.log('multiplier = ' + multiplier);
+
+              var newAmount = amount * multiplier;
+              var newUnits = coinTypes[coinShift];
+              console.log('newAmount = ' + newAmount);
+              console.log('newUnits = ' + newUnits);
+
+              var robyBucks = exchangeCoinValues(newAmount, newUnits);
+              console.log('robyBucks = ' + robyBucks[0] + ' ' + robyBucks[1]);
+
+              return robyBucks[0] + ' ' + robyBucks[1];
+
             }
-
-            console.log(convert('5', 'gp'));
 
             var moneyData = $('tr:has(td.money.number) .money.number');
 
             moneyData.each(function processMoney(index, moneyTD) {
-              console.log(moneyTD.innerHTML);
               var moneyArray = moneyTD.innerHTML.split(' ');
               var amount = parseInt(moneyArray[0].replace(/,/g, ""));
               var units = moneyArray[1];
-              console.log(amount + ', ' + units);
               $(moneyTD).append(' => ' + convert(amount, units)).css('background', 'red');
             });
 
